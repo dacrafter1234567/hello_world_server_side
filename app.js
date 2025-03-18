ec0106151ba9e0ac15792e5763f1af63d3eedbf4
dacrafter1234567 <dacrafter247@gmail.com>
2025-03-18 09:54:48 -0400

    better text

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const port = process.env.PORT || 3001;
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("Hello World");
+    res.end("We're no strangers to love. You know the rules, and so do I. A full commitment's what I'm looking for. You wouldn't get this from any other guy. I just wanna tell you how I'm feeling. Gotta make you understand. Never gonna give you up. Never gonna let you down. Never gonna run around and desert you. Never gonna make you cry. Never gonna say goodbye. Never gonna tell a lie and hurt you.");
   })
   .listen(port);
