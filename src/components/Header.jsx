806ed5311ec24a0032f9647aee2f0be95629260d
dacrafter1234567 <dacrafter247@gmail.com>
2025-03-27 09:02:32 -0400

    help

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import "./css/Header.css";
+import Navigation from "./Navigation";
+
+function Header() {
+    return(
+        <header>
+            <h1>Title of the Site</h1>
+        </header>
+    );
+};
+
+export default Header;
